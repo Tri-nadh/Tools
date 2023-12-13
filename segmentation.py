062076d015d617cf127e0c33a40cff53b9d11036
Eswar <89859338+Tri-nadh@users.noreply.github.com>
2023-12-14 00:37:56 +0530

    Add files via upload

---
+++ segmentation.py
@@ -0,0 +1,81 @@
+#required libraries
+from PIL import Image, ImageDraw, ImageFont
+import streamlit as st
+import numpy as np
+import cv2
+
+st.set_page_config(
+    page_title='Image Tools',
+    page_icon='🔍')
+tab1, tab2 = st.tabs([
+   "Segmentation",
+   "Quote"
+])
+with tab1:
+	st.header("Image Segmentation With Kmeans", divider='rainbow')
+	uploaded_file = st.file_uploader("Upload image", type=['jpeg', 'png', 'jpg', 'webp'])
+            
+	if uploaded_file is not None:
+		file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+		image = cv2.imdecode(file_bytes, 1)
+	#image = cv2.imread(image)
+	k = int(st.text_input('Type a number',0))
+	st.write('The current number is', k)
+
+	#reading image
+	if st.button('Get Photo'):
+		image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+		pixel_vals = image.reshape((-1,3))
+		pixel_vals = np.float32(pixel_vals)
+
+#
+		criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 100, 0.85)
+
+#performing Kmeans
+		retval, labels, centers = cv2.kmeans(pixel_vals, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+		centers = np.uint8(centers)
+		segmented_data = centers[labels.flatten()]
+
+
+		segmented_image = segmented_data.reshape((image.shape))
+	
+	#Performing branding
+
+	
+
+		st.image(segmented_image)
+with tab2:
+	st.header("Instagram Quote Creator", divider='rainbow')
+	#quote = st.text_input("Enter the Quote:")
+	quote = st.text_area("Enter the Quote:")
+
+# Create a new image with white background
+	image = Image.open("/home/shiva/Downloads/Content/Quote/quote.jpg")
+	width, height = image.width, image.height
+# Create a drawing object
+	draw = ImageDraw.Draw(image)
+
+# Define the text to be written
+	text = str(quote)
+
+# font
+	size = int(st.text_input("Enter the font size"))
+	font = ImageFont.truetype('/home/shiva/Downloads/Nova_Square/NovaSquare-Regular.ttf', size)
+
+# Calculate the position to center the text
+#text_width, text_height = draw.textlength(text, font)
+	wp =int( st.text_input("Enter the width Percent", key =height))
+#hlkafaosifj
+	hp =int( st.text_input("Enter the height Percent"))
+	x = width - ((width * wp)/100)
+#x = width //3
+#y = height // 2
+	y = height- ((height * hp)/100)
+# Write the text on the image
+	draw.text((x, y), text, font=font, fill ="black", align ="center")
+
+# Save the image
+#image.save("/home/shiva/Downloads/output.jpg")
+	st.image(image)
+
